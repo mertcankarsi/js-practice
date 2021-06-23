378ddee2216a6e9093159fc55562baa1878dde06
Mert Can Karsi <karsi.mertcan@gmail.com>
2021-06-23 16:23:04 +0300

    updated algorithm for unsuccessful tests

--- pairwise.js
+++ pairwise.js
@@ -1,13 +1,19 @@
 function pairwise(arr, arg) {
-    var result = 0;
-    for(let i=0; i<arr.length-1; i++) {
-      for(let j=i; j<arr.length; j++) {
-        if(arr[i] + arr[j] == arg)
-          result += i + j;
+  var result = 0;
+  var pairs = [];
+  for(let i=0; i<arr.length-1; i++) {
+    for(let j=i+1; j<arr.length; j++) {
+      if(arr[i]==null || arr[j]==null) 
+        continue;
+      if(arr[i] + arr[j] == arg){
+        arr[i]=null;
+        arr[j]=null;
+        pairs.push([i, j]);
+        result += i + j;
       }
     }
-    console.log(result);
-    return result;
   }
-  
-  pairwise([1,4,2,3,0,5], 7);
+  return result;
+}
+
+pairwise([1, 3, 2, 4], 4);
