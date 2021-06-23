cc2a9ebb4c6f725aa1006810d96e67cedac7d1f1
Mert Can Karsi <karsi.mertcan@gmail.com>
2021-06-23 22:00:10 +0300

    reformat

--- pairwise.js
+++ pairwise.js
@@ -1,13 +1,13 @@
 function pairwise(arr, arg) {
   var result = 0;
   var pairs = [];
-  for(let i=0; i<arr.length-1; i++) {
-    for(let j=i+1; j<arr.length; j++) {
-      if(arr[i]==null || arr[j]==null) 
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] == null || arr[j] == null)
         continue;
-      if(arr[i] + arr[j] == arg){
-        arr[i]=null;
-        arr[j]=null;
+      if (arr[i] + arr[j] == arg) {
+        arr[i] = null;
+        arr[j] = null;
         pairs.push([i, j]);
         result += i + j;
       }
